69b5ca9300aa1a7f6d2426cfec0776f52a84dea2
nPu3ePBaTuB <hot-boost@mail.ru>
2016-10-18 18:11:14 +0300

    Тест таба наполнение

--- buld/js/history.js
+++ buld/js/history.js
@@ -43,83 +43,89 @@
 
 
 
-
-
-
 //
-var m = [];
-var m2 = [];
- function newArr() {
-
-     var TOTAL_PAINTING = parseInt($('#TOTAL_PAINTING_ID').val());
-     if (isNaN(TOTAL_PAINTING)){
-         TOTAL_PAINTING = 0;
-     }
-     // =
-     // - Создали внутренние массивы
-     if (TOTAL_PAINTING > 0){
-         for (i = 0; i <= TOTAL_PAINTING-1; i++) {
-             m[i] = new Array(4);
-         }
-     }
-     // =
-     // - Массив со всеми данными
-     for (var i = 0; i < TOTAL_PAINTING; i++) {
-
-         var karkasName = $('*[data-id=' + (i + 1) + ']').find('.karkas-name').text();
-         if (karkasName == 'Optimax2') {
-             selectedKarkas = 'OptimaхTwo';
-         } else if (karkasName == 'Statusx1') {
-             selectedKarkas = 'StatusхOne';
-         } else if (karkasName == 'Statusx2') {
-             selectedKarkas = 'StatusхTwo';
-         } else {
-             selectedKarkas = karkasName;
-         }
-
-         m[i][0] = selectedKarkas;
-         m[i][1] = parseInt($('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-price') );
-         m[i][2] = parseInt($('*[data-id="' + (i + 1 ) + '"]').attr('data-width') ) + parseInt(( $('*[data-id="' + (i + 1 ) + '"]').attr('data-height'))) ;
-         m[i][3] =
-             $('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-tsvet-uplotnitelya') + ',' +
-             $('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-tsvet-zaglushki') + ',' +
-             $('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-tsvet-zaglushki-tortsevoy') + ',' +
-             $('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-vid-krepleniya');
-     }
-     for (var i = 0; i < TOTAL_PAINTING; i++) {
-         if (m2.length == 0){
-             m2.push(m[i][0]);
-             m2.push(m[i][1]);
-             m2.push(m[i][2]);
-             m2.push(m[i][3]);
-         }
-         else {
-             var status = true;
-             var price = 0;
-             for (var j = 0; j < m2.length; j++) {
-                 if (m2[j] == m[i][0]) {
-                     for (var g = m2.length; g >= 0; g--) {
-                         if (m2[g] == m[i][0]){
-                             m2[j+1] = parseInt(m2[j+1]) + parseInt(m[i][1]);
-                             m2[j+2] = parseInt(m2[j+2]) + parseInt(m[i][2]);
-                         }
-                     }
-                     status = false;
-                 }
-             }
-             if (status == true){
-                 m2.push(m[i][0]);
-                 m2.push(m[i][1]);
-                 m2.push(m[i][2]);
-                 m2.push(m[i][3]);
-             }
-         }
-     }
-     for(var i = 0; i<= m2.length; i=i+3){
-         console.log( '--------------------' )
-         console.log( i, m2[i] )
-         console.log( i, m2[i+1] )
-         console.log( i, m2[i+2] )
-         console.log( '--------------------' )
-     }
- }
+//
+//
+////
+//var m = [];
+//var m2 = [];
+// function newArr() {
+//
+//     var TOTAL_PAINTING = parseInt($('#TOTAL_PAINTING_ID').val());
+//     if (isNaN(TOTAL_PAINTING)){
+//         TOTAL_PAINTING = 0;
+//     }
+//     // =
+//     // - Создали внутренние массивы
+//     if (TOTAL_PAINTING > 0){
+//         for (i = 0; i <= TOTAL_PAINTING-1; i++) {
+//             m[i] = new Array(4);
+//         }
+//     }
+//     // =
+//     // - Массив со всеми данными
+//     for (var i = 0; i < TOTAL_PAINTING; i++) {
+//
+//         var karkasName = $('*[data-id=' + (i + 1) + ']').find('.karkas-name').text();
+//         if (karkasName == 'Optimax2') {
+//             selectedKarkas = 'OptimaхTwo';
+//         } else if (karkasName == 'Statusx1') {
+//             selectedKarkas = 'StatusхOne';
+//         } else if (karkasName == 'Statusx2') {
+//             selectedKarkas = 'StatusхTwo';
+//         } else {
+//             selectedKarkas = karkasName;
+//         }
+//
+//         m[i][0] = selectedKarkas;
+//         m[i][1] = parseInt($('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-price') );
+//         m[i][2] = parseInt($('*[data-id="' + (i + 1 ) + '"]').attr('data-width') ) + parseInt(( $('*[data-id="' + (i + 1 ) + '"]').attr('data-height'))) ;
+//         m[i][3] =
+//             $('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-tsvet-uplotnitelya') + ',' +
+//             $('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-tsvet-zaglushki') + ',' +
+//             $('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-tsvet-zaglushki-tortsevoy') + ',' +
+//             $('*[data-id="' + (i + 1 ) + '"]').attr('data-karkas-vid-krepleniya');
+//     }
+//     for (var i = 0; i < TOTAL_PAINTING; i++) {
+//         if (m2.length == 0){
+//             m2.push(m[i][0]);
+//             m2.push(m[i][1]);
+//             m2.push(m[i][2]);
+//             m2.push(m[i][3]);
+//         }
+//         else {
+//             var status = true;
+//             var price = 0;
+//             for (var j = 0; j < m2.length; j++) {
+//                 if (m2[j] == m[i][0]) {
+//                     for (var g = m2.length; g >= 0; g--) {
+//                         if (m2[g] == m[i][0]){
+//                             m2[j+1] = parseInt(m2[j+1]) + parseInt(m[i][1]);
+//                             m2[j+2] = parseInt(m2[j+2]) + parseInt(m[i][2]);
+//                         }
+//                     }
+//                     status = false;
+//                 }
+//             }
+//             if (status == true){
+//                 m2.push(m[i][0]);
+//                 m2.push(m[i][1]);
+//                 m2.push(m[i][2]);
+//                 m2.push(m[i][3]);
+//             }
+//         }
+//     }
+//     for(var i = 0; i<= m2.length; i=i+3){
+//         console.log( '--------------------' )
+//         console.log( i, m2[i] )
+//         console.log( i, m2[i+1] )
+//         console.log( i, m2[i+2] )
+//         console.log( '--------------------' )
+//     }
+// }
+function loadMoadP(){
+    var text = $('.ss').html();
+    $('#DIAGRAMMA-DIALOG-WINDOW .modal-body').html("");
+    $('#DIAGRAMMA-DIALOG-WINDOW .modal-body').html(text);
+    $("#DIAGRAMMA-DIALOG-WINDOW").modal('toggle');
+}
